be6fdfbb3ca50fd0016b184502c19c187cde3aa2
Jahanvi Singh <115407422+jahanvi513@users.noreply.github.com>
2024-04-15 17:05:17 +0530

    Delete isc_sport.sql

--- isc_sport.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `sport`;
-CREATE TABLE `sport` (
-  `id` int NOT NULL,
-  `Type` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-LOCK TABLES `sport` WRITE;
-UNLOCK TABLES;
